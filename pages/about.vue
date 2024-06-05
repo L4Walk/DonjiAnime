a097fa56a826783d2b1d5143a04a61e9170cb0fd
L4Walk <L4Walk@chuheng.tech>
2024-06-06 00:11:26 +0800

    完善

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
     <main class="container mx-auto py-8">
-      <h1 class="text-3xl font-bold mb-4">About Us</h1>
-      <p class="text-lg">
-        Welcome to our About Us page. Here we describe our company, mission, and
-        values.
-      </p>
+      <div class="lg:flex">
+        <div class="w-full h-64 lg:w-1/2 lg:h-auto">
+          <div class="w-full h-full bg-cover"
+               style="background-image: url(https://images.unsplash.com/photo-1508394522741-82ac9c15ba69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=748&q=80)">
+            <div class="w-full h-full bg-black opacity-25"></div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center w-full px-6 py-8 lg:h-[32rem] lg:w-1/2">
+          <div class="max-w-xl">
+            <h1 class="text-3xl font-semibold text-customRed lg:text-4xl">About Us </h1>
+
+            <p class="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base"><span class="text-customRed ">Donji Anime</span>（PT East Collection Indonesia）, located in Jakarta, is a
+              Globally enterprise established in 2023. We focus on CardGame R&D,
+              ACGN Goods Retail, Cultural and Creative Industries Investment,
+              Entertainment Live Broadcast.</p>
+            <p class="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base">Our founding team upholds the concept of cultural diversity and
+              youthfulness. Core members covering various sectors of the cultural
+              industry, including top companies like Tesla, Ogilvy, Louis Vuitton,
+              Tsuburaya Productions, and Bilibili.</p>
+
+            <p class="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base">We adhere to a localization strategy and have assembled a sales and
+              brand operations team of over twenty people, with key members
+              coming from local renowned enterprises such as Indoritel Makmur, F
+              commerce, and Mixue Indonesia.</p>
+            <p class="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base">Donji Anime established a comprehensive network of online and
+              offline retail and distribution, and set up platforms for influencer. The CCG category hold a top 5 market share in Indonesia and
+              Malaysia. We pioneers the card unboxing category on Indonesian
+              TikTok. We has established extensive connections with Southeast
+              Asian IPs and internet platforms through partnerships with Chinese
+              entities like Mobile Legends, Monsta, TLG, Indomaret and TikTok.</p>
+          </div>
+        </div>
+
+      </div>
     </main>
   </div>
 </template>
--- pages/products.vue
+++ pages/products.vue
@@ -1,8 +1,55 @@
 <template>
   <div>
     <main class="container mx-auto py-8">
-      <h1 class="text-3xl font-bold mb-4">Products</h1>
-      <p class="text-lg">Discover our range of products.</p>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="h-[32rem] bg-gray-100 dark:bg-gray-800">
+          <div class="container px-6 py-10 mx-auto">
+            <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Products</h1>
+
+            <div class="flex justify-center mx-auto mt-6">
+              <span class="inline-block w-40 h-1 bg-customRed rounded-full"></span>
+              <span class="inline-block w-3 h-1 mx-1 bg-customRed rounded-full"></span>
+              <span class="inline-block w-1 h-1 bg-customRed rounded-full"></span>
+            </div>
+
+            <p class="max-w-2xl mx-auto mt-6 text-center text-gray-500 dark:text-gray-300">
+              We've launched three exclusive agency products in Indonesia: One Piece series, Sanrio series, and Pokémon series.
+            </p>
+            <p class="max-w-2xl mx-auto mt-6 text-center text-gray-500 dark:text-gray-300">
+              Each of our products comes with different reward positions, making your card collecting hobby not only more fun but also more profitable.
+            </p>
+            <p class="max-w-2xl mx-auto mt-6 text-center text-gray-500 dark:text-gray-300">
+              In the fourth quarter of 2024, Donji will launch the Card game of Southeast Asia's top IP, Mobile Legends,
+              and conduct public testing of online blind box machines and live streaming platforms. Stay tuned for updates.
+            </p>
+          </div>
+        </div>
+
+        <div class="container px-6 py-10 mx-auto -mt-72 sm:-mt-80 md:-mt-96">
+          <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-3">
+            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700">
+              <img class="object-cover w-full rounded-xl aspect-square"
+                   src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                   alt="">
+
+            </div>
+
+            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700">
+              <img class="object-cover w-full rounded-xl aspect-square"
+                   src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                   alt="">
+
+            </div>
+
+            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700">
+              <img class="object-cover w-full rounded-xl aspect-square"
+                   src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                   alt="">
+
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   </div>
 </template>
